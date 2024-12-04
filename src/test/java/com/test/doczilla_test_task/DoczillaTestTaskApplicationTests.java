db7397eef6e92af7cb7471edd00045af1828dc19
Danila Osipov <d.osssipov@gmail.com>
2024-12-04 14:17:32 +0400

    [Task-2] Add template for web project

---
+++ src/test/java/com/test/doczilla_test_task/DoczillaTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.doczilla_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoczillaTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
